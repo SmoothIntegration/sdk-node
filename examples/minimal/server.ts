1a17b293df7ae88e4da440d745f8630710f8317b
Thimo Visser <thimo.visser@gmail.com>
2025-01-23 10:50:58 +0700

    bumped sdk version in example & removed example id, secrets, and company id from minimal example

--- examples/minimal/server.ts
+++ examples/minimal/server.ts
@@ -6,23 +6,20 @@ import Client, { Event } from '@smooth-integration/sdk-node';
 const app = express();
 
 // Create a SmoothIntegration client, make sure read the API key from a safe place like a .env file
-const client: Client = new Client(
-    'b032518b-aef6-4742-a235-7ff8c784e51f',
-    'ozmFguhGsi3IkHXhxyjz6wqK696FzYjnbabNqISc1u0',
-);
+const client: Client = new Client('<your_client_id>', '<your_client_secret>');
 
 // Replace with the companyId you registered for this user on SmoothIntegration
-const demoCompanyId = 'dacbb9dd-9f27-471d-b0ce-b07d80fa8a18';
+const demoCompanyId = '<your_company_id>';
 
 // Create a route to connect QuickBooks
-app.get('/connect-xero', async (_: Request, res: Response) => {
-    const consentUrl = await client.xero.getConsentUrl(demoCompanyId);
+app.get('/connect-quickbooks', async (_: Request, res: Response) => {
+    const consentUrl = await client.quickBooks.getConsentUrl(demoCompanyId);
     res.redirect(consentUrl);
 });
 
 // Start the HTTP server
 app.listen(8080, () => {
-    console.log(`Server running, connect Xero at http://localhost:8080/connect-xero`);
+    console.log(`Server running, connect QuickBooks at http://localhost:8080/connect-quickbooks`);
 });
 
 // Log Events as they come in
